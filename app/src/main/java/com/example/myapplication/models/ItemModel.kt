17bd5879d62a2ceb27e80c1379a4615262c7cd6e
Angelina Maurer <116002806+angi-mrer@users.noreply.github.com>
2023-08-30 19:23:50 +0200

    Update ItemModel.kt

--- app/src/main/java/com/example/myapplication/models/ItemModel.kt
+++ app/src/main/java/com/example/myapplication/models/ItemModel.kt
@@ -2,7 +2,15 @@ package com.example.myapplication
 
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-
+class DataModel(){
+        private val database = Firebase.database
+        private val myRef = database.getReference("item")
+    
+        fun saveData(newData: NewData){
+            val newDataRef = myRef.push()
+            newDataRef.setValue(newData)
+        }
+    }
 data class NewItem(
     val gefundenText: String,
     val beschreibungText: String,
